c8c991c1d63cd30fb4888bba3fb222c5afd5a29b
Ewenblitz <ewen.lomer@ynov.com>
2018-06-03 18:34:42 +0200

    Remise en place du cheminement du site + début de *seen*

--- app/controllers/tomes_controller.rb
+++ app/controllers/tomes_controller.rb
@@ -1,2 +1,10 @@
 class TomesController < ApplicationController
+
+  def show
+    @tomes = Tome.all
+    @tomes = Tome.find(params[:id])
+
+    @chapters = Chapter.all
+    @chapters = Chapter.where(tome_id: @tomes)
+  end
 end
